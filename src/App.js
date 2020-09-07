078de1c5f60f5dd5c6b734b62c5f962c91e72941
damu <damu@example.com>
2020-09-07 15:20:00 +0800

    引入并配置路由

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 // 应用的根组件，简单组件用函数定义，复杂组件用类定义，根据有没有状态来区分
 import React, {Component} from "react";
-import {Button} from "antd";
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+// 引入组件，首字母必须大写
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 import './App.less';
 
 export default class App extends Component {
   // 类组件必须有一个render方法，render方法必须返回一个虚拟DOM对象，用jsx的标签语法来写虚拟DOM对象
   render() {
-    return <Button type="primary">Primary Button</Button>;
+    return (
+      <BrowserRouter>
+        <Switch>{/*只匹配其中一个*/}
+          <Route path='/login' component={Login}/>
+          <Route path='/' component={Admin}/>
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
